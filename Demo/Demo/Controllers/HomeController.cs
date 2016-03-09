644c4787244d93b300d2e1150595ad242140662d
Blake Truong <blake.truong@tpssoft.com>
2016-03-09 11:33:42 +0700

    Commit demo project

---
+++ Demo/Demo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DemoDatabaseEntities demoContext = new DemoDatabaseEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ListProduct()
+        {
+            var listProducts = new List<Product>();
+            if (demoContext != null)
+                listProducts = demoContext.Products.ToList();
+            return View(listProducts);
+        }
+    }
+}
